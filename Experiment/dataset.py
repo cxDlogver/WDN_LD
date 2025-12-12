020451dd48eea4ccb30b93b0cd00ba187e7985ba
cxDlogver <2801726619@qq.com>
2025-12-12 22:13:43 +0800

    RFLOMA Source Code

--- Experiment/dataset.py
+++ Experiment/dataset.py
@@ -1,6 +1,5 @@
 """ 数据集加载 """
 import pandas as pd
-
 from model import *
 
 # 2018年无泄漏数据
@@ -69,53 +68,53 @@ with open(os.path.join(File_PATH, "leak_demands.csv"), "w", newline="") as f:
     leak_demands.to_csv(f, index=False)
 
 # 真实场景泄漏数据
-# filename = "real_dataset_2018"
-# read_dataset(dir, filename)
-# print("真实场景2018年泄漏数据加载完成")
-#
-# filename = "real_no_dataset_2018"
-# read_dataset(dir, filename)
-# print("真实场景2018年无泄漏数据加载完成")
-#
-# _, _, _, real_pressure_2018 = get_dataset(f"{File_PATH}/real_no_dataset_2018.pickle",
-#                                           f"{File_PATH}/real_dataset_2018.pickle")
-# with open(os.path.join(File_PATH, "real_pressure_2018.csv"), "w", newline="") as f:
-#     real_pressure_2018.to_csv(f, index=True)
-# with open(os.path.join(File_PATH, "real_pressure_2018.pickle"), "wb") as f:
-#     pickle.dump(real_pressure_2018, f)
+filename = "real_dataset_2018"
+read_dataset(dir, filename)
+print("真实场景2018年泄漏数据加载完成")
+
+filename = "real_no_dataset_2018"
+read_dataset(dir, filename)
+print("真实场景2018年无泄漏数据加载完成")
+
+_, _, _, real_pressure_2018 = get_dataset(f"{File_PATH}/real_no_dataset_2018.pickle",
+                                          f"{File_PATH}/real_dataset_2018.pickle")
+with open(os.path.join(File_PATH, "real_pressure_2018.csv"), "w", newline="") as f:
+    real_pressure_2018.to_csv(f, index=True)
+with open(os.path.join(File_PATH, "real_pressure_2018.pickle"), "wb") as f:
+    pickle.dump(real_pressure_2018, f)
 
 
 
-# filename = "real_dataset_2019"
-# read_dataset(dir, filename)
-# print("真实场景2019年泄漏数据加载完成")
-#
-# filename = "real_no_dataset_2019"
-# read_dataset(dir, filename)
-# print("真实场景2019年无泄漏数据加载完成")
-#
-# _, _, _, real_pressure_2019 = get_dataset(f"{File_PATH}/real_no_dataset_2019.pickle",
-#                                           f"{File_PATH}/real_dataset_2019.pickle")
-#
-# with open(os.path.join(File_PATH, "real_pressure_2019.csv"), "w", newline="") as f:
-#     real_pressure_2019.to_csv(f, index=True)
-# with open(os.path.join(File_PATH, "real_pressure_2019.pickle"), "wb") as f:
-#     pickle.dump(real_pressure_2019, f)
+filename = "real_dataset_2019"
+read_dataset(dir, filename)
+print("真实场景2019年泄漏数据加载完成")
 
-#
-# filename = "real_dataset_2018"
-# read_dataset(dir, filename)
-# print("真实场景2018年泄漏数据加载完成")
-#
-# filename = "real_no_dataset_2018"
-# read_dataset(dir, filename)
-# print("真实场景2018年无泄漏数据加载完成")
-#
-# _, _, _, real_pressure_2018 = get_dataset(f"{File_PATH}/real_no_dataset_2018.pickle",
-#                                           f"{File_PATH}/real_dataset_2018.pickle")
-#
-# with open(os.path.join(File_PATH, "real_pressure_2018.pickle"), "wb") as f:
-#     pickle.dump(real_pressure_2018, f)
+filename = "real_no_dataset_2019"
+read_dataset(dir, filename)
+print("真实场景2019年无泄漏数据加载完成")
+
+_, _, _, real_pressure_2019 = get_dataset(f"{File_PATH}/real_no_dataset_2019.pickle",
+                                          f"{File_PATH}/real_dataset_2019.pickle")
+
+with open(os.path.join(File_PATH, "real_pressure_2019.csv"), "w", newline="") as f:
+    real_pressure_2019.to_csv(f, index=True)
+with open(os.path.join(File_PATH, "real_pressure_2019.pickle"), "wb") as f:
+    pickle.dump(real_pressure_2019, f)
+
+
+filename = "real_dataset_2018"
+read_dataset(dir, filename)
+print("真实场景2018年泄漏数据加载完成")
+
+filename = "real_no_dataset_2018"
+read_dataset(dir, filename)
+print("真实场景2018年无泄漏数据加载完成")
+
+_, _, _, real_pressure_2018 = get_dataset(f"{File_PATH}/real_no_dataset_2018.pickle",
+                                          f"{File_PATH}/real_dataset_2018.pickle")
+
+with open(os.path.join(File_PATH, "real_pressure_2018.pickle"), "wb") as f:
+    pickle.dump(real_pressure_2018, f)
 
 
 # 2018 和 2019 数据 合并
